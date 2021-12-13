4655d244b31523e1487b4db7b28c64421b913cf6
oleg <o.a.vypritskij@samsmu.ru>
2021-12-13 16:41:32 +0400

    copy repository

---
+++ Assets/CameraShtun.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShtun : MonoBehaviour
+{
+    [SerializeField] private float m_Speed, m_Ampl;
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0 , Mathf.Sin(Time.time * m_Speed) * m_Ampl, 0));
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private DateTime m_TimeToNY;
+
+    [SerializeField] private Text m_Days, m_Hours, m_Minutes, m_Seconds;
+
+    private TimeSpan m_TimeLeft;
+    
+    void Start()
+    {
+        m_TimeToNY = new DateTime(2022,01,01,00,00,00);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_TimeLeft = m_TimeToNY - DateTime.Now;
+        m_Days.text = m_TimeLeft.Days.ToString();
+        m_Hours.text = m_TimeLeft.Hours.ToString();
+        m_Minutes.text = m_TimeLeft.Minutes.ToString();
+        m_Seconds.text = m_TimeLeft.Seconds.ToString();
+    }
+}
