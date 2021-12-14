d6d2424c7c3824e7ee3d66a320f693a71046bbf8
SAMSMU\ovypritskiy <ovypritskiy@samsmu.local>
2021-12-14 08:12:42 +0400

    added mute controleer

---
+++ Assets/SoundController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class SoundController : MonoBehaviour
+{
+   [SerializeField] private VideoPlayer m_Player;
+   [SerializeField] private bool m_IsOn;
+   private int m_Count;
+
+
+   public void Mute()
+   {
+      Debug.Log("click");
+      if (m_Count == 0)
+      {
+         m_Player.SetDirectAudioMute(0, true);
+         m_Count++;
+      }
+      else
+      {
+         m_Player.SetDirectAudioMute(0, false);
+         m_Count = 0;
+      }
+   }
+}
