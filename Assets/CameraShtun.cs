98669db68338a3bc326acbeaf812647962a28a1b
Анатолий Константинов <a.d.konstantinov@samsmu.ru>
2021-12-15 14:12:07 +0400

    Add channel change ( K button ), Add trees and snow on background

--- Assets/CameraShtun.cs
+++ Assets/CameraShtun.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class CameraShtun : MonoBehaviour
 {
+    public VideoPlayer TvPlayer;
+    public VideoClip[] clip;
     [SerializeField] private float m_Speed, m_Ampl;
     void Update()
     {
         transform.rotation = Quaternion.Euler(new Vector3(0 , Mathf.Sin(Time.time * m_Speed) * m_Ampl, 0));
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TvPlayer.clip = clip[Random.Range(0, clip.Length)];
+        }
     }
 }
